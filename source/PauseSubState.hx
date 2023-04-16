407ed419e6d7254ce74a785fe3d133a45a1a7714
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-16 21:54:42 +0300

    update source/PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -129,7 +129,9 @@ class PauseSubState extends MusicBeatSubstate
 		changeSelection();
 
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+                #if mobile addVirtualPad(UP_DOWN, A_B); #end
 	}
+
 	function regenMenu() 
 	{
         
