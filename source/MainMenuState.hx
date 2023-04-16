a59ac8a7798593bbdcf619b514297296f38f3369
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-16 19:53:08 +0300

    update source/MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -13,7 +13,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import io.newgrounds.NG;
+//import io.newgrounds.NG;
 import lime.app.Application;
 
 #if windows
